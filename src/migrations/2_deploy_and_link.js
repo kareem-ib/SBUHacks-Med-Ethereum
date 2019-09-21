36625101f8a9d0b83ac7ef9bd1837d40f2d8ebf4
Monty Bichouna <othmanbichouna@gmail.com>
2019-09-21 11:23:47 -0400

    contracts test deploy

--- src/migrations/2_deploy_and_link.js
+++ src/migrations/2_deploy_and_link.js
@@ -2,36 +2,44 @@ const Info = artifacts.require("Info");
 const PatientInfo = artifacts.require("PatientInfo");
 const Patient = artifacts.require("Patient");
 const Doctor = artifacts.require("Doctor");
+const DoctorFactory = artifcats.require("DoctorFactory");
+const PatientFactory = artifcats.require("PatientFactory");
+
 
 const config = require('../config.js');
 const doctor = config.doctor;
 const patient = config.patient;
 
 module.exports = function(deployer, network, accounts) {
-    deployer.deploy(Info).then(()=>deployer.link(Info, [PatientInfo, Patient, Doctor]));
+    deployer.deploy(Info);
+	deployer.link(Info, [PatientInfo, Patient, Doctor, DoctorFactory, PatientFactory]);
     // const info = await Info.new()
     // await PatientInfo.link(Info, info.address);
     // const m = await PatientInfo.new(patient.dob, patient.dob, patient.dob, patient.dob, patient.dob, patient.dob);
 	//deployer.link(Info, [PatientInfo, Patient, Doctor]);
 
-    console.log('SSFF', patient.fname);
-    deployer.deploy(Patient, 
-	patient.mnemonic,
-	patient.fname,
-	patient.lname,
-	patient.dob,
-	patient.ethnicity,
-	patient.patientAddress,
-	{ from: accounts[0] }
-    );
-    deployer.deploy(Doctor,
-	doctor.mnemonic,
-	doctor.fname,
-	doctor.lname,
-	doctor.fullName,
-	doctor.organization,
-	doctor.medical_license,
-	doctor.specialization,
-	{ from: accounts[1] }
-    );
+    // console.log('SSFF', patient.fname);
+    // deployer.deploy(Patient, 
+	// patient.mnemonic,
+	// patient.fname,
+	// patient.lname,
+	// patient.dob,
+	// patient.ethnicity,
+	// patient.patientAddress,
+	// { from: accounts[0] }
+    // );
+    // deployer.deploy(Doctor,
+	// doctor.mnemonic,
+	// doctor.fname,
+	// doctor.lname,
+	// doctor.fullName,
+	// doctor.organization,
+	// doctor.medical_license,
+	// doctor.specialization,
+	// { from: accounts[1] }
+	// );
+	
+	deployer.deploy(DoctorFactory);
+	deployer.deploy(PatientFactory);
+
 };
